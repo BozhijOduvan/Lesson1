307ab4c2e1e682cff778219ab65350dc7349908d
BozhijOduvan <114654818+BozhijOduvan@users.noreply.github.com>
2022-09-29 12:45:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Lesson1;
+
+Draw.RectangleFromTop(100, 2, 10, 11, ConsoleColor.White);
+
+Draw.RectangleFromTop(100, 2, 10, 15, ConsoleColor.Blue);
+
+Draw.RectangleFromTop(100, 2, 10, 19, ConsoleColor.Red);
+
+
+
+
